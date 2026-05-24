b672bc181ea9cad840dc7f011b55b027216ffef6
oTarakna <164069689+oTarakna@users.noreply.github.com>
2026-05-24 13:20:37 -0400

    add menu.py

---
+++ Questões/1 . Sistema_Multimidia/ui/menu.py
@@ -0,0 +1,9 @@
+def mostrarMenu():
+
+    print("\n========== MENU ==========")
+    print("1 - Adicionar Vídeo")
+    print("2 - Adicionar Podcast")
+    print("3 - Adicionar Texto Narrado")
+    print("4 - Listar Mídias")
+    print("5 - Reproduzir Todas")
+    print("0 - Sair")
