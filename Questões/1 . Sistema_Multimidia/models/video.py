22aa75092fa4a870d4921e3355209ba0de914156
oTarakna <164069689+oTarakna@users.noreply.github.com>
2026-05-24 13:02:19 -0400

    add video.py

---
+++ Questões/1 . Sistema_Multimidia/models/video.py
@@ -0,0 +1,11 @@
+from midia import Midia
+from abc import ABC, abstractmethod
+
+#CLASSE VIDEO
+class Video(Midia):
+    def __init__(self, titulo, duracao, resolucao):
+        super().__init__(titulo, duracao)
+        self.resolucao = resolucao
+
+    def reproduzir(self):
+        print(f"O Video está sendo reproduzido, \nTitulo = {self.titulo}, Resolucao = {self.resolucao}")
