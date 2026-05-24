f8d7cda370e2988dae6ae120e86e8d1406dd4769
oTarakna <164069689+oTarakna@users.noreply.github.com>
2026-05-24 16:42:42 -0400

    Update armazenador.py

--- Questões/5.Sistema_de_Armazenamento/models/armazenador.py
+++ Questões/5.Sistema_de_Armazenamento/models/armazenador.py
@@ -1,18 +1,11 @@
 from abc import ABC, abstractmethod
-from models.salvavel import Salvavel
 
 
 class Armazenador(ABC):
-    def __init__(self,dado):
+
+    def __init__(self, dado):
         self.dado = dado
 
-    def salvar(dado):
+    @abstractmethod
+    def salvar(self):
         pass
-
-
-
-def executarSalvamentoFormal(Armazenador: Armazenador, dado):
-    Armazenador.salvar(dado)
-
-def executarSalvamentoFlexivel(objeto: Salvavel, dado):
-    objeto.salvar(dado)
