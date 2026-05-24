c31686c13eb569636fb9d7f4fa3f5407f684e1d5
oTarakna <164069689+oTarakna@users.noreply.github.com>
2026-05-24 13:52:05 -0400

    Create menu.py

---
+++ Questões/4.Sistema_de_Impressao/ui/menu.py
@@ -0,0 +1,8 @@
+def mostrar_menu():
+
+    print("\n========== MENU ==========")
+    print("1 - Criar Boleto")
+    print("2 - Criar Etiqueta")
+    print("3 - Criar Relatório")
+    print("4 - Imprimir Tudo")
+    print("0 - Sair")
