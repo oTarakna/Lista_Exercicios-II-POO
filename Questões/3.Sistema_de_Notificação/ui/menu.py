2b1ce2b1cc88c3c38bc5543a1804df80b15b4a4e
oTarakna <164069689+oTarakna@users.noreply.github.com>
2026-05-24 13:46:20 -0400

    Create menu.py

---
+++ Questões/3.Sistema_de_Notificação/ui/menu.py
@@ -0,0 +1,8 @@
+def mostrar_menu():
+
+    print("\n========== MENU ==========")
+    print("1 - Adicionar Email")
+    print("2 - Adicionar SMS")
+    print("3 - Adicionar App")
+    print("4 - Enviar Notificação")
+    print("0 - Sair")
