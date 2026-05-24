eeeb56a4d1e772ddb01af35d83df7bbcc0e651cd
oTarakna <164069689+oTarakna@users.noreply.github.com>
2026-05-24 13:36:25 -0400

    Create menu.py

---
+++ Questões/2.Sistema_de_Empresa/ui/menu.py
@@ -0,0 +1,8 @@
+def mostrarMenu():
+
+    print("\n========== MENU ==========")
+    print("1 - Adicionar Funcionário Assalariado")
+    print("2 - Adicionar Funcionário Horista")
+    print("3 - Adicionar Funcionário Comissionado")
+    print("4 - Mostrar Folha de Pagamento")
+    print("0 - Sair")
