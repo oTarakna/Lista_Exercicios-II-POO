2b39179d837c32b0046d7214ed4789f1646084c1
oTarakna <164069689+oTarakna@users.noreply.github.com>
2026-05-24 16:47:30 -0400

    Create menu.py

---
+++ Questões/5.Sistema_de_Armazenamento/ui/menu.py
@@ -0,0 +1,8 @@
+def mostrar_menu():
+
+    print("\n========== MENU ==========")
+    print("1 - Salvar em Arquivo")
+    print("2 - Salvar em Banco")
+    print("3 - Salvar em Nuvem")
+    print("4 - Processar Tudo")
+    print("0 - Sair")
